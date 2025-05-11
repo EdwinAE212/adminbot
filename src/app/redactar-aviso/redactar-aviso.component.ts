18a9c5a71888f335979ca97c042cab97b931fbef
EdwinAE212 <113155807+EdwinAE212@users.noreply.github.com>
2025-05-11 15:40:44 -0700

    Agregué componente reporte y cambie el redactar aviso

--- src/app/redactar-aviso/redactar-aviso.component.ts
+++ src/app/redactar-aviso/redactar-aviso.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-redactar-aviso',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './redactar-aviso.component.html',
   styleUrl: './redactar-aviso.component.css'
 })
 export class RedactarAvisoComponent {
   imagePreview: string | ArrayBuffer | null = null;
   selectedImage: File | null = null;
+  fechaCaducidad: string = '';
 
   onImageSelected(event: any): void {
     const file = event.target.files[0];
     if (file) {
       this.selectedImage = file;
-
       const reader = new FileReader();
       reader.onload = () => {
         this.imagePreview = reader.result;
@@ -26,10 +27,14 @@ export class RedactarAvisoComponent {
   }
 
   onSubmit(): void {
+    console.log('Fecha de caducidad:', this.fechaCaducidad);
+
     if (this.selectedImage) {
       console.log('Imagen seleccionada:', this.selectedImage);
-      // Aquí puedes armar un FormData para enviar al backend
+      // Puedes usar FormData para enviar al backend si es necesario
     }
+
     // Aquí iría el resto del procesamiento del formulario
   }
 }
+
---
+++ src/app/reporte/reporte.component.ts
@@ -0,0 +1,35 @@
+  import { Component } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { FormsModule } from '@angular/forms';
+  
+  @Component({
+    selector: 'reporte',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './reporte.component.html',
+    styleUrl: './reporte.component.css'
+  })
+  export class ReporteComponent {
+    imagePreview: string | ArrayBuffer | null = null;
+    selectedImage: File | null = null;
+  
+    onImageSelected(event: any): void {
+      const file = event.target.files[0];
+      if (file) {
+        this.selectedImage = file;
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.imagePreview = reader.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  
+    onSubmit(): void {
+      if (this.selectedImage) {
+        console.log('Imagen seleccionada:', this.selectedImage);
+      }
+      // Procesar resto del formulario
+    }
+  }
+  
