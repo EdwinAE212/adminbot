57eb26fa0411e02f4a6e5cf37810384be7906355
EdwinAE212 <113155807+EdwinAE212@users.noreply.github.com>
2025-11-26 22:13:43 -0700

    avance ahora si

--- src/app/reporte/reporte.component.ts
+++ src/app/reporte/reporte.component.ts
@@ -9,7 +9,7 @@
     templateUrl: './reporte.component.html',
     styleUrl: './reporte.component.css'
   })
-  export default class ReporteComponent {
+  export class ReporteComponent {
     imagePreview: string | ArrayBuffer | null = null;
     selectedImage: File | null = null;
   
